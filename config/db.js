d031115f7ec3e9716547460f6505a39b6493d39c
Ioan <ionutt93@gmail.com>
2014-11-02 23:35:02 +0000

    again 2

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
     dev_url: 'mongodb://localhost:27017/development',
-    prod_url: 'mongodb://ioan:905e0bec6fd0babb6a88bf56050053b4@dogen.mongohq.com:10062/app31190733',
+    prod_url: 'mongodb://ioan:troll4life@dogen.mongohq.com:10062/app31190733',
     test_url: 'mongodb://localhost:27017/test'
 };
