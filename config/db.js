1483ae8978e13a414e9327b036048494ca4d61fe
Ioan <ionutt93@gmail.com>
2014-11-02 23:24:39 +0000

    changed prod url again 2

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
     dev_url: 'mongodb://localhost:27017/development',
-    prod_url: 'mongodb://<heroku>:<1377e5b074429f175f57b9798afd49c6>@dogen.mongohq.com:10062/app31190733',
+    prod_url: 'mongodb://@dogen.mongohq.com:10062/app31190733',
     test_url: 'mongodb://localhost:27017/test'
 };
