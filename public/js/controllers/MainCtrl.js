2ce2f1ca992ee28fc84ee6e927561fac9034490f
Ioan Troana <ionutt93@gmail.com>
2015-05-19 23:33:29 +0100

    extra changes

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,6 +1,8 @@
 'use strict'
-angular.module("MainCtrl", []).controller("MainController", ['$scope', "$window", function ($scope, $window) {
+angular.module("MainCtrl", []).controller("MainController", ['$scope', "$window", '$location', function ($scope, $window, $location) {
     $scope.tagline = 'To the moon and back!';
+    $scope.isLoggedIn = false;
+    $scope.isLoginScreen = false;
 
     if (window.location.pathname.indexOf("login") != -1) {
     	$scope.backgroundImage = {
@@ -14,5 +16,17 @@ angular.module("MainCtrl", []).controller("MainController", ['$scope', "$window"
     $scope.logout = function () {
         delete $window.sessionStorage.token;
         $window.location.href = '/login';
+    }
+
+    if ($window.sessionStorage.token) {
+        $scope.isLoggedIn = true;
+    } else {
+        $scope.isLoggedIn = false;
+    }
+
+    if ($location.path() == '/login') {
+        $scope.isLoginScreen = true;
+    } else {
+        $scope.isLoginScreen = false;
     }
 }]);
